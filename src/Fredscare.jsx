718be9be78268f83065660a905139d4d0c036428
AmahleZuma <170299648+AmahleZuma@users.noreply.github.com>
2025-12-10 00:55:04 +0200

    Getting the hang of events and the beginning of state

--- src/Fredscare.jsx
+++ src/Fredscare.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
+
 export default function jumpScare() {
 
-  function fredScare() {
-      alert('Freddy has killed you')
-  }
+ const [amount, setAmount] = useState(0);
+ function addKill(){
+    setAmount(amount + 1);
 
+    if (amount !== 1) {
+        alert('Freddy has killed you ' + amount + ' times.' );
+    } else {
+        alert('Freddy has killed you ' + amount + ' time.' );
+    }
+    
+    };
+
+
+    
 
   return (
-    <button onClick={fredScare}>
+    <button onClick={addKill}>
       Door
     </button>
   );
 }
+
+// so there is state which is data that sets what the site will be like (appearance or function)
+// you say for example state and setState, setState is for when you want to change it
+// does not have to be these above names specifically but the format is something an setSomething
