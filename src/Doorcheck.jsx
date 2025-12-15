0cad8ed2389918d4d2300d582ffba6b3e7cc01f6
AmahleZuma <170299648+AmahleZuma@users.noreply.github.com>
2025-12-15 19:03:00 +0200

    Added useSound hook, it effectively plays the door sound.

--- src/Doorcheck.jsx
+++ src/Doorcheck.jsx
@@ -1,22 +1,35 @@
 import {useState} from "react";
+import {useSound} from 'use-sound';
 import './Securityroom.css'
+import doorClose from './sfx/doorOpen.mp3';
 
 export default function doorCheck(){
     const [doorStatus1, setdoorStatus1] = useState("Closed");
     // If you dont make a separate variable, both elements are affected at once
     const [doorStatus2, setdoorStatus2] = useState("Closed");
 
+    // This plays the door sound
+    const [playdoorClose] = useSound(doorClose);
+
+
+
     function changeDoor1() {
         setdoorStatus1(prev =>
             prev === "Closed" ? "Opened" : "Closed"
         );
-        
+
+        // Door close sound effect
+        playdoorClose();
+            
     }
 
     function changeDoor2() {
         setdoorStatus2(prev =>
             prev === "Closed" ? "Opened" : "Closed"
         );
+
+        // Door close sound effect
+        playdoorClose();
     }
 
 
@@ -28,7 +41,7 @@ export default function doorCheck(){
                 <div className="security-room-container">
                     <button onClick={changeDoor1} className="button">Door 1 {doorStatus1}</button>
                     <div className="security-room">
-
+                    
                     </div>
                     <button onClick={changeDoor2} className="button">Door 2 {doorStatus2}</button>
                 </div>
